179710b8959fd12b9067804af2bf565bcd77c676
Marc Touw <marc.touw@students.bfh.ch>
2016-01-08 12:30:10 +0100

    minor changes

--- client/templates/exercise-search.js
+++ client/templates/exercise-search.js
@@ -8,5 +8,12 @@ Template.exerciseSearch.helpers(
 		},
 		i18nDificulty: function (difficulty) {
 			return "i18n \"difficulty.".concat(difficulty.toString(),"\"");
+		},
+		i18nExercises: function() {
+			if (i18n.getLanguage() == en) {
+				return "names.[0].name";
+			} else if (i18n.getLanguage() == de){
+				return "names.[1].name";
+			}
 		}
 	});
