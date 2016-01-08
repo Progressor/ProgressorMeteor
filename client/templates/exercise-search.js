05774d5b9764552872bec9b34b5b163bf032266e
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-01-08 13:20:03 +0100

    i18n exercise-search.js

--- client/templates/exercise-search.js
+++ client/templates/exercise-search.js
@@ -1,19 +1,16 @@
 Template.exerciseSearch.helpers(
 	{
-		dificulties: function () {
+		difficulties: function () {
 			return difficulties;
 		},
 		exercises: function (difficulty) {
 			return Exercises.find({ difficulty: difficulty });
 		},
-		i18nDificulty: function (difficulty) {
-			return "i18n \"difficulty.".concat(difficulty.toString(),"\"");
+		i18nDifficulty: function (difficulty) {
+			return i18n('difficulty.' + difficulty);
 		},
-		i18nExercises: function() {
-			if (i18n.getLanguage() == en) {
-				return "names.[0].name";
-			} else if (i18n.getLanguage() == de){
-				return "names.[1].name";
-			}
+		i18nExerciseName: function (exercise) {
+			return (_.findWhere(exercise.names, { language: i18n.getLanguage() })
+							|| _.findWhere(exercise.names, { language: i18n.getDefaultLanguage() })).name;
 		}
 	});
