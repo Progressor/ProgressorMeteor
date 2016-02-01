1ab273d415ff75b2cc78f077cb873e20040943ab
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-02-01 16:11:25 +0100

    enhance programming-exercise\edit (functions column)

--- client/templates/programming-exercise/edit.js
+++ client/templates/programming-exercise/edit.js
@@ -1,56 +1,153 @@
 (function () {
 	'use strict';
 
-	var exercise, executorTypes, executorTypesDependency = new Tracker.Dependency();
+	var defExercise = {
+		functions: [{ inputNames: [''] }],
+		testCases: [{}]
+	};
 
-	Template.programmingEdit.onCreated(function () {
-		exercise = Progressor.exercises.findOne();
-		Session.set('ProgrammingLanguage', exercise ? exercise.programmingLanguage : null);
+	var exercise, depExercise = new Tracker.Dependency();
+	var executorTypes, depExecutorTypes = new Tracker.Dependency();
+
+	function testExecutorIdentifier(val) {
+		return val.length === 0 || /^[A-Z_][A-Z0-9_]*$/i.test(val);
+	}
+
+	function testExecutorType(val, rec) {
+		var typ = _.find(executorTypes.types, typ => val.substr(0, typ._id.length) === typ._id), idx = typ ? typ._id.length : 0;
+		if (!typ) return false;
+		else if (typ.parameterCount > 0) {
+			if (val.substr(idx, 1) !== "<") return false;
+			idx += 1;
+			for (var i = 0; i < typ.parameterCount; i++) {
+				var dlm = i === typ.parameterCount - 1 ? '>' : ',', inc;
+				if (i > 0) idx += val.substr(idx).match(/^\s?/)[0].length;
+				if ((inc = testExecutorType(val.substr(idx), true)) === false) return false;
+				idx += inc;
+				if (val.substr(idx, 1) !== dlm) return false;
+				idx += dlm.length;
+			}
+		}
+		return rec ? idx : idx === val.length;
+	}
+
+	Template.programmingEdit.onCreated(() => {
+		exercise = Progressor.exercises.findOne() || defExercise;
+		depExercise.changed();
+
 		Meteor.call('getExecutorTypes', (err, res) => {
 			executorTypes = res;
-			executorTypesDependency.changed();
+			depExecutorTypes.changed();
 		});
 	});
 
 	Template.programmingEdit.helpers(
 		{
-			exerciseSearchData: () => ({ _id: exercise.programmingLanguage }),
-			i18nProgrammingLanguage: () => i18n.getProgrammingLanguage(exercise.programmingLanguage),
-			i18nCategoryName: i18n.getName,
-			i18nExerciseName: i18n.getName,
-			i18nDifficulty: i18n.getDifficulty,
-			languages: () => _.map(i18n.getLanguages(), (nam, id) => ({
-				_id: id, name: nam, isActive: id === i18n.getLanguage(),
-				exerciseName: i18n.getNameForLanguage(exercise, id),
-				exerciseDescription: i18n.getDescriptionForLanguage(exercise, id)
-			})),
-			executorTypes: () => {
-				executorTypesDependency.depend();
-				return executorTypes;
+			exerciseSearchData() {
+				depExercise.depend();
+				return { _id: exercise.programmingLanguage };
 			},
-			i18nProgrammingLanguages: () => _.map(Progressor.getProgrammingLanguages(), lng => ({
-				_id: lng, name: i18n.getProgrammingLanguage(lng),
-				isActive: exercise && lng === exercise.programmingLanguage
-			})),
-			i18nCategories: () => Progressor.categories.find({ programmingLanguage: Session.get('ProgrammingLanguage') }).map(cat => _.extend(cat, {
-				name: i18n.getName(cat),
-				isActive: exercise && cat._id === exercise.category_id
-			})),
-			i18nDifficulties: () => _.map(Progressor.getDifficulties(), dif => ({ _id: dif, name: i18n.getDifficulty(dif), isActive: exercise && dif === exercise.difficulty }))
+			i18nProgrammingLanguage() {
+				depExercise.depend();
+				return i18n.getProgrammingLanguage(exercise.programmingLanguage);
+			},
+			i18nExerciseName: i18n.getName,
+			i18nCategoryName: i18n.getName,
+			i18nDifficulty: i18n.getDifficulty,
+			i18nProgrammingLanguages() {
+				depExercise.depend();
+				return _.map(Progressor.getProgrammingLanguages(), lng => ({
+					_id: lng, name: i18n.getProgrammingLanguage(lng),
+					isActive: exercise && lng === exercise.programmingLanguage
+				}));
+			},
+			i18nCategories() {
+				depExercise.depend();
+				return Progressor.categories.find({ programmingLanguage: exercise.programmingLanguage }).map(cat => _.extend(cat, {
+					name: i18n.getName(cat),
+					isActive: cat._id === exercise.category_id
+				}));
+			},
+			i18nDifficulties() {
+				depExercise.depend();
+				return _.map(Progressor.getDifficulties(), dif => ({
+					_id: dif, name: i18n.getDifficulty(dif),
+					isActive: dif === exercise.difficulty
+				}));
+			},
+			i18nExerciseNamesDescriptions() {
+				depExercise.depend();
+				return _.map(i18n.getLanguages(), (nam, id) => ({
+					_id: id, language: nam, isActive: id === i18n.getLanguage(),
+					name: i18n.getNameForLanguage(exercise, id),
+					description: i18n.getDescriptionForLanguage(exercise, id)
+				}));
+			},
+			executorTypes() {
+				depExecutorTypes.depend();
+				return executorTypes ? executorTypes.types : [];
+			},
+			executorValues: () => {
+				depExecutorTypes.depend();
+				return executorTypes ? _.map(executorTypes.values, val => _.extend(val, { typeLabels: val.types.join(', ') })) : [];
+			},
+			functions(cas) {
+				depExercise.depend();
+				return _.map(exercise.functions, ex => {
+					return _.extend(ex, {
+						outputType: ex.outputTypes ? ex.outputTypes[0] : null,
+						inputs: _.map(ex.inputNames && ex.inputNames.length ? ex.inputNames : [{}], (nme, idx) => ({ name: nme, type: ex.inputTypes ? ex.inputTypes[idx] : null })),
+						isActive: cas && cas.functionName === ex.name
+					});
+				});
+			},
+			testCases() {
+				depExercise.depend();
+				return exercise.testCases;
+			}
 		});
 
 	Template.programmingEdit.events(
 		{
-			'click a[data-toggle="tab"]'(ev) {
-				ev.preventDefault();
-				$(ev.currentTarget).tab('show');
-			},
 			'click .dyn-repeat'(ev) {
-				var $this = $(ev.currentTarget), $rep = $this.closest('.dyn-repeatable');
-				$rep.after($rep.clone().find('input').val(null).end());
+				var $this = $(ev.currentTarget), $rep = $this.closest('.dyn-repeatable'), $cln = $rep.clone(true).insertAfter($rep);
 				$this.remove();
+				$cln.data('index', $cln.data('index') + 1);
+				$cln.find('.dyn-repeatable').data('index', 0).prev('.dyn-repeatable').remove();
+				$cln.find('input').val(null);
+				$cln.find('select').each((idx, elm) => $(elm).val($(elm).find('option').first().val()));
+				$cln.find('input, select').each((idx, elm) => {
+					var $elm = $(elm), id = $elm.data('id');
+					$elm.parents('.dyn-repeatable').each((idy, par) => id = id.replace(`{${$(par).data('var')}}`, $(par).data('index')));
+					$cln.find(`[for="${$elm.prop('id')}"]`).prop('for', id);
+					$elm.prop('id', id);
+				});
 			},
-			'change #select-language': ev => Session.set('ProgrammingLanguage', $(ev.currentTarget).val())
+			'change #select-language'(ev) {
+				exercise.programmingLanguage = $(ev.currentTarget).val();
+				depExercise.changed();
+			},
+			'keyup [id^="input-function-name-"]'(ev) {
+				var $this = $(ev.currentTarget), $grp = $this.closest('.form-group');
+				var $grps = $('[id^="input-function-name-"]').closest('.form-group').removeClass('has-error').end();
+				if (!testExecutorIdentifier($this.val()))
+					$grp.addClass('has-error');
+				_.each(_.flatten(_.filter(_.groupBy($grps, elm => $(elm).val()), grp => grp.length > 1)),
+							 elm => $(elm).closest('.form-group').addClass('has-error'));
+			},
+			'keyup [id^="input-parameter-name-"]'(ev) {
+				var $this = $(ev.currentTarget), $grp = $this.closest('.form-group'), $fnc = $this.closest('.container-function');
+				var $grps = $fnc.find('[id^="input-parameter-name-"]').closest('.form-group').removeClass('has-error').end();
+				if (!testExecutorIdentifier($this.val()))
+					$grp.addClass('has-error');
+				_.each(_.flatten(_.filter(_.groupBy($grps, elm => $(elm).val()), grp => grp.length > 1)),
+							 elm => $(elm).closest('.form-group').addClass('has-error'));
+			},
+			'keyup .exec-type'(ev) {
+				var $this = $(ev.currentTarget), $grp = $this.closest('.form-group').removeClass('has-error');
+				if (!testExecutorType($this.val()))
+					$grp.addClass('has-error');
+			}
 		});
 
 })();
--- server/executor.js
+++ server/executor.js
@@ -13,22 +13,33 @@
 	Meteor.methods(
 		{
 			getExecutorTypes() {
-				return [
-					{ _id: ttypes.TypeString, label: ttypes.TypeString, parameterCount: 0 },
-					{ _id: ttypes.TypeCharacter, label: ttypes.TypeCharacter, parameterCount: 0 },
-					{ _id: ttypes.TypeBoolean, label: ttypes.TypeBoolean, parameterCount: 0 },
-					{ _id: ttypes.TypeByte, label: ttypes.TypeByte, parameterCount: 0 },
-					{ _id: ttypes.TypeShort, label: ttypes.TypeShort, parameterCount: 0 },
-					{ _id: ttypes.TypeInteger, label: ttypes.TypeInteger, parameterCount: 0 },
-					{ _id: ttypes.TypeLong, label: ttypes.TypeLong, parameterCount: 0 },
-					{ _id: ttypes.TypeSingle, label: ttypes.TypeSingle, parameterCount: 0 },
-					{ _id: ttypes.TypeDouble, label: ttypes.TypeDouble, parameterCount: 0 },
-					{ _id: ttypes.TypeDecimal, label: ttypes.TypeDecimal, parameterCount: 0 },
-					{ _id: ttypes.TypeContainerArray, label: `${ttypes.TypeContainerArray}<T>`, parameterCount: 1 },
-					{ _id: ttypes.TypeContainerList, label: `${ttypes.TypeContainerList}<T>`, parameterCount: 1 },
-					{ _id: ttypes.TypeContainerSet, label: `${ttypes.TypeContainerSet}<T>`, parameterCount: 1 },
-					{ _id: ttypes.TypeContainerMap, label: `${ttypes.TypeContainerMap}<K, V>`, parameterCount: 2 }
-				];
+				return {
+					types: [
+						{ _id: ttypes.TypeString, label: ttypes.TypeString, parameterCount: 0 },
+						{ _id: ttypes.TypeCharacter, label: ttypes.TypeCharacter, parameterCount: 0 },
+						{ _id: ttypes.TypeBoolean, label: ttypes.TypeBoolean, parameterCount: 0 },
+						{ _id: ttypes.TypeByte, label: ttypes.TypeByte, parameterCount: 0 },
+						{ _id: ttypes.TypeShort, label: ttypes.TypeShort, parameterCount: 0 },
+						{ _id: ttypes.TypeInteger, label: ttypes.TypeInteger, parameterCount: 0 },
+						{ _id: ttypes.TypeLong, label: ttypes.TypeLong, parameterCount: 0 },
+						{ _id: ttypes.TypeSingle, label: ttypes.TypeSingle, parameterCount: 0 },
+						{ _id: ttypes.TypeDouble, label: ttypes.TypeDouble, parameterCount: 0 },
+						{ _id: ttypes.TypeDecimal, label: ttypes.TypeDecimal, parameterCount: 0 },
+						{ _id: ttypes.TypeContainerArray, label: `${ttypes.TypeContainerArray}<T>`, parameterCount: 1 },
+						{ _id: ttypes.TypeContainerList, label: `${ttypes.TypeContainerList}<T>`, parameterCount: 1 },
+						{ _id: ttypes.TypeContainerSet, label: `${ttypes.TypeContainerSet}<T>`, parameterCount: 1 },
+						{ _id: ttypes.TypeContainerMap, label: `${ttypes.TypeContainerMap}<K, V>`, parameterCount: 2 }
+					],
+					values: [
+						{ types: [ttypes.TypeString], values: ['Progressor', 'Bern University of Applied Sciences'] },
+						{ types: [ttypes.TypeCharacter], values: ['p', 'P', '<'] },
+						{ types: [ttypes.TypeBoolean], values: [true, false] },
+						{ types: [ttypes.TypeByte, ttypes.TypeShort, ttypes.TypeInteger, ttypes.TypeLong], values: [-25, 0, 1, new Date().getYear()] },
+						{ types: [ttypes.TypeSingle, ttypes.TypeDouble, ttypes.TypeDecimal], values: [-3.141592653, 0, 1, 3.141592653] },
+						{ types: [ttypes.TypeContainerArray, ttypes.TypeContainerList, ttypes.TypeContainerSet], values: ['-1,0,1,2', 'strut1,touwm1,weidj1'] },
+						{ types: [ttypes.TypeContainerMap], values: ['strut1:Thomas Strub,touwm1:Marc Touw,weidj1:Janick Weidmann'] }
+					]
+				};
 			},
 			getBlacklist(language) {
 				check(language, String);
