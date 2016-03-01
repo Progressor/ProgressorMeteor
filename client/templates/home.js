f99b2cc1b8b14fd3987c5c5360b78b394e43db18
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-03-01 18:10:38 +0100

    fix intro, hide 'home' navlink

--- client/templates/home.js
+++ client/templates/home.js
@@ -8,19 +8,22 @@
 		return _.filter(_.map(cols, (no, cod) => (idx + 1) % (12 / no) === 0 ? cod : null), cod => !!cod);
 	}
 
-	var introIndex = 0;
-	(function introAnimation() {
-		if (introIndex < introTexts[0].length) {
-			$('#introTitle').text(introTexts[0].substr(0, ++introIndex));
-			setTimeout(introAnimation, 200);
-		} else if (introIndex < introTexts[0].length + introTexts[1].length) {
-			$('#introExplanation').text(introTexts[1].substr(0, ++introIndex - introTexts[0].length));
-			setTimeout(introAnimation, 200);
-		} else {
-			$('<span id="introUnderscore">_</span>').hide().fadeIn(500).appendTo($('#introExplanation'));
-			setInterval(() => $('#introUnderscore').fadeOut(500).fadeIn(500), 1000);
-		}
-	})();
+	Template.home.onRendered(() => {
+		var introIndex = 0;
+		var introInterval = setInterval(function () {
+			if (introIndex < introTexts[0].length)
+				$('#intro').text(introTexts[0].substr(0, ++introIndex)); //animate title
+			else if (introIndex < introTexts[0].length + introTexts[1].length) {
+				if (introIndex == introTexts[0].length)
+					$('<small id="introExplanation"></small>').appendTo($('#intro')); //create & animate explanation
+				$('#introExplanation').text(introTexts[1].substr(0, ++introIndex - introTexts[0].length));
+			} else {
+				clearInterval(introInterval); //clear animation interval
+				$('<span id="introUnderscore">_</span>').hide().fadeIn(500).appendTo($('#introExplanation'));
+				setInterval(() => $('#introUnderscore').fadeOut(500).fadeIn(500), 1000); //create & animate underscore
+			}
+		}, 200);
+	});
 
 	Template.home.helpers(
 		{
@@ -35,9 +38,6 @@
 				var cnt = Progressor.exercises.find({ programmingLanguage: lng }).count();
 				return `${cnt} ${i18n(`exercise.exercise${cnt != 1 ? 's' : ''}`)}`;
 			},
-			getSVG (lng) {
-				return $.parseHTML(svg[lng]);
-			},
 			i18nProgrammingLanguage: i18n.getProgrammingLanguage,
 			i18nProgrammingLanguageDescription: i18n.getProgrammingLanguageDescription
 		}
@@ -45,9 +45,9 @@
 
 	Template.home.events(
 		{
-			'click [data-href]': (ev) => Router.go('exerciseSearch', { _id: $(ev.currentTarget).data('href') }),
-			'mouseover .panel-default:not(.disabled)': (ev) => $(ev.currentTarget).removeClass('panel-default').addClass('panel-primary'),
-			'mouseout .panel-primary': (ev) => $(ev.currentTarget).removeClass('panel-primary').addClass('panel-default')
+			'click [data-href]': ev => Router.go('exerciseSearch', { _id: $(ev.currentTarget).data('href') }),
+			'mouseover .panel-default:not(.disabled)': ev => $(ev.currentTarget).removeClass('panel-default').addClass('panel-primary'),
+			'mouseout .panel-primary': ev => $(ev.currentTarget).removeClass('panel-primary').addClass('panel-default')
 		});
 
 })();
