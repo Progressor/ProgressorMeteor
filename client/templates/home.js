f099abad22884ef6d20d2bee225f60a83ffeca85
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-03-02 08:58:32 +0100

    fix intro, hide 'home' navlink

--- client/templates/home.js
+++ client/templates/home.js
@@ -2,18 +2,17 @@
 	'use strict';
 
 	const cols = { xs: 6, sm: 4, md: 3, lg: 2 };
-	const introTexts = [i18n('layout.logo'), i18n('layout.explanation')];
+	const introTexts = [i18n('layout.logo') + ' ', i18n('layout.explanation')];
 
 	function getSeparators(idx) {
 		return _.filter(_.map(cols, (no, cod) => (idx + 1) % (12 / no) === 0 ? cod : null), cod => !!cod);
 	}
 
-	Template.home.onRendered(
-		() => {
+	Template.home.onRendered(() => {
 		var introIndex = 0;
 		var introInterval = setInterval(function () {
 			if (introIndex < introTexts[0].length)
-				$('#intro').text(introTexts[0].substr(0, ++introIndex) + ' '); //animate title
+				$('#intro').text(introTexts[0].substr(0, ++introIndex)); //animate title
 			else if (introIndex < introTexts[0].length + introTexts[1].length) {
 				if (introIndex == introTexts[0].length)
 					$('<small id="introExplanation"></small>').appendTo($('#intro')); //create & animate explanation
@@ -23,7 +22,7 @@
 				$('<span id="introUnderscore">_</span>').hide().fadeIn(500).appendTo($('#introExplanation'));
 				setInterval(() => $('#introUnderscore').fadeOut(500).fadeIn(500), 1000); //create & animate underscore
 			}
-		}, 200);
+		}, 150);
 	});
 
 	Template.home.helpers(
