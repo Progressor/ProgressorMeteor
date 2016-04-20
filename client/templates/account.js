7ae855b3e1c8736583698f0d475f6e614951d405
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-04-20 10:39:41 +0200

    allow authorisation of administrators

--- client/templates/account.js
+++ client/templates/account.js
@@ -5,13 +5,25 @@
 	 * MAIN TEMPLATE
 	 */
 
-	Template.account.onRendered(() => $('#collapseArchive').on('show.bs.collapse hide.bs.collapse', ev => $(ev.currentTarget).siblings().find('.glyphicon').toggleClass('glyphicon-plus-sign glyphicon-minus-sign')));
+	Template.account.onCreated(function () {
+		this.userValues = [];
+	});
+
+	Template.account.onRendered(function () {
+		$('#collapseArchive').on('show.bs.collapse hide.bs.collapse', ev => $(ev.currentTarget).siblings().find('.glyphicon').toggleClass('glyphicon-plus-sign glyphicon-minus-sign'));
+		Meteor.typeahead.inject();
+	});
 
 	Template.account.helpers(
 		{
 			currentUserEmail: () => Progressor.getUserEmail(Meteor.user()),
 			currentUserName: () => Progressor.getUserName(Meteor.user(), true),
-			transformResults: r => _.map(r, i => _.extend({ result: _.omit(i, 'exercise') }, i.exercise))
+			transformResults: r => _.map(r, i => _.extend({ result: _.omit(i, 'exercise') }, i.exercise)),
+			users: () => _.map(Meteor.users.find({ roles: { $elemMatch: Progressor.ROLE_ADMIN } }).fetch(), function (user) {
+				const value = [Progressor.getUserName(user, true), Progressor.getUserEmail(user)].join(' ');
+				Template.instance().userValues[value] = user;
+				return { value: value, name: Progressor.getUserName(user, true), email: Progressor.getUserEmail(user) };
+			})
 		});
 
 	Template.account.events(
@@ -24,6 +36,19 @@
 					$group.addClass(!err ? 'has-success' : 'has-error');
 					Meteor.setTimeout(() => $group.removeClass('has-success has-error'), 500);
 				});
+			},
+			'click #button-make-admin'() {
+				const $input = $('#input-make-admin'), $group = $input.closest('.form-group'), user = Template.instance().userValues[$input.val()];
+				if (user)
+					Meteor.call('toggleUsersRoles', [user._id], [Progressor.ROLE_ADMIN], true, Progressor.handleError(function (err) {
+						$group.addClass(!err ? 'has-success' : 'has-error');
+						Meteor.setTimeout(function () {
+							$group.removeClass('has-success has-error');
+							$input.val(null);
+						}, 500);
+					}));
+				else
+					Progressor.showAlert(i18n('form.noSelectionMessage'));
 			}
 		});
 
