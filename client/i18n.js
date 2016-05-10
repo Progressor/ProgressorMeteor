e60f6e9765951509014097d76b1f76ccd2814a0c
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-05-10 13:08:15 +0200

    save default language

--- client/i18n.js
+++ client/i18n.js
@@ -1,23 +1,27 @@
 (function () {
 	'use strict';
 
-	function setLanguage(lng) {
-		return _.some(i18n.getLanguages(), (name, code) => {
-			if (lng.startsWith(code)) {
-				i18n.setLanguage(code, true);
-				return true;
-			}
-			return false;
+	function setLanguage(local = false) {
+		return language => _.some(i18n.getLanguages(), (name, code) => {
+			const match = language.startsWith(code);
+			if (match) i18n.setLanguage(code, local);
+			return match;
 		});
 	}
 
 	Meteor.startup(function () {
-		_.some(navigator.languages || [navigator.language, navigator.userLanguage, navigator.browserLanguage, navigator.systemLanguage], setLanguage);
+
+		const languages = navigator.languages || [navigator.language, navigator.userLanguage, navigator.browserLanguage, navigator.systemLanguage];
+
+		//set default language (don't save yet)
+		_.some(languages, setLanguage(true));
+
 		Tracker.autorun(() => {
 			const usr = Meteor.user();
-			if (usr && usr.profile && usr.profile.language)
-				setLanguage(usr.profile.language);
+			if (!usr || !usr.profile || !usr.profile.language || !setLanguage(true)(usr.profile.language)) //set profile language (don't overwrite)
+				_.some(languages, setLanguage()); //save default language
 		});
+
 	});
 
 })();
