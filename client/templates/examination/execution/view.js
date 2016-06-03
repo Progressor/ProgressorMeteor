47cda1c5ceb22c2432d4680754e6f7ee9f13e40f
Marc Touw <marc.touw@students.bfh.ch>
2016-06-03 15:15:40 +0200

    view template data

--- client/templates/examination/execution/view.js
+++ client/templates/examination/execution/view.js
@@ -6,13 +6,16 @@
 	}
 
 	Template.examinationExecutionView.onCreated(function () {
-
 	});
 
 	Template.examinationExecutionView.helpers(
 		{
 			exercises: () => _.map(Progressor.executions.findOne().exercises, (e, i) => _.extend({ weight: e.weight }, Progressor.joinCategory(Progressor.exercises.findOne({ _id: e.exercise_id })))),
-			exerciseNumber: (num) => num + 1,
+			totalWeight: () => {
+				let totalWeight = 0;
+				_.each(Progressor.executions.findOne().exercises, e => totalWeight += e.weight);
+				return totalWeight;
+			}
 		});
 
 	Template.examinationExecutionView.events(
