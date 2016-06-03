3eb09b8e49faf8cf558708401a52f85f9181a366
Marc Touw <marc.touw@students.bfh.ch>
2016-06-03 13:12:20 +0200

    fix execution methods

--- server/collections/executions.js
+++ server/collections/executions.js
@@ -13,13 +13,13 @@
 						exercises: [Match.ObjectIncluding({ base_id: String, weight: Number })]
 					}));
 
-				const _examination = execution._id ? Progressor.examinations.findOne({ _id: execution._id }) : execution;
+				const _executions = execution._id ? Progressor.executions.findOne({ _id: execution._id }) : execution;
 
 				if (!this.userId)
 					throw new Meteor.Error('not-authenticated', i18n.forUser('error.notAuthenticated.message', this.userId));
-				else if (_examination._id && _examination.author_id !== this.userId && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
+				else if (_executions._id && _executions.author_id !== this.userId && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
 					throw new Meteor.Error('not-owner', i18n.forUser('error.notAuthor.message', this.userId));
-				else if (_examination._id && _examination.released && _examination.released.requested && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
+				else if (_executions._id && _executions.released && _executions.released.requested && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
 					throw new Meteor.Error('not-admin', i18n.forUser('error.notAdmin.message', this.userId));
 
 				if (!execution.author_id)
@@ -27,37 +27,39 @@
 				execution.lastEditor_id = this.userId;
 				execution.lastEdited = new Date();
 
-				return Progressor.examinations.upsert(execution._id, execution).insertedId || execution._id;
+				return Progressor.executions.upsert(execution._id, execution).insertedId || execution._id;
 			},
 			deleteExecution(execution) {
 				check(execution, Match.ObjectIncluding({ _id: String }));
 
 				if (execution._id)
-					execution = Progressor.examinations.findOne({ _id: execution._id });
+					execution = Progressor.executions.findOne({ _id: execution._id });
 
 				if (!this.userId)
 					throw new Meteor.Error('not-authenticated', i18n.forUser('error.notAuthenticated.message', this.userId));
 				else if (execution._id && execution.author_id !== this.userId && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
 					throw new Meteor.Error('not-owner', i18n.forUser('error.notAuthor.message', this.userId));
 
-				return Progressor.examinations.remove(execution._id).rowsAffected;
+				return Progressor.executions.remove(execution._id).rowsAffected;
 			},
 			startExecution(execution) {
 				check(execution, Match.ObjectIncluding({ _id: String }));
 
 				if (execution._id)
-					execution = Progressor.examinations.findOne({ _id: execution._id });
+					execution = Progressor.executions.findOne({ _id: execution._id });
 
 				if (!this.userId)
 					throw new Meteor.Error('not-authenticated', i18n.forUser('error.notAuthenticated.message', this.userId));
 				else if (execution._id && execution.author_id !== this.userId && !Roles.userIsInRole(this.userId, Progressor.ROLE_ADMIN))
 					throw new Meteor.Error('not-owner', i18n.forUser('error.notAuthor.message', this.userId));
 
-				return Progressor.examinations.update(execution._id, {
+				return Progressor.executions.update(execution._id, {
 					$set: {
 						startTime: new Date(),
-						exercises: _.match(execution.exercises, exercise => _.extend(exercise, {
-							exercise_id: Progressor.exercises.insert(_.omit(exercise, '_id', 'category_id', 'difficulty')).insertedId
+						exercises: _.map(execution.exercises, exercise => _.extend(exercise, {
+							exercise_id: Progressor.exercises.insert(_.extend(_.omit(Progressor.exercises.find({ _id: exercise._id }), '_id', 'category_id', 'difficulty'), {
+								execution_id: execution._id
+							})).insertedId
 						}))
 					}
 				}).rowsAffected;
