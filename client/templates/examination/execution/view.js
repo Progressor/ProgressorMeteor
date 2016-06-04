47f9ef8ab2ea2fe200084844bcf3365e6be1185d
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-06-04 21:34:42 +0200

    review recent changes (1/2)

--- client/templates/examination/execution/view.js
+++ client/templates/examination/execution/view.js
@@ -1,22 +1,13 @@
 (function () {
 	'use strict';
 
-	function tmpl() {
-		return Template.instance();
-	}
-
 	Template.examinationExecutionView.helpers(
 		{
 			exercises: () => _.map(Progressor.executions.findOne().exercises, (e, i) => _.extend({ weight: e.weight }, Progressor.joinCategory(Progressor.exercises.findOne({ _id: e.exercise_id })))),
-			totalWeight: () => {
-				let totalWeight = 0;
-				_.each(Progressor.executions.findOne().exercises, e => totalWeight += e.weight);
-				return totalWeight;
-			},
-			noSuccessFeedback: (e) => e.type == 3 && e.pattern != null,
-			success: (e, r) => Progressor.isExerciseSuccess(e, r),
-			isSolved: (e, r) => Progressor.isExerciseEvaluated(e, r)
-			
+			totalWeight: () => _.reduce(Progressor.executions.findOne().exercises, (w, e) => w + e.weight, 0),
+			noSuccessFeedback: e => e.type == 3 && !e.pattern,
+			evaluated: (e, r) => Progressor.isExerciseEvaluated(e, r),
+			successPercentage: (e, r) => Progressor.getExerciseSuccessPercentage(e, r)
 		});
 
 })();
