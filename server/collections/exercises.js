6820e848c41c22355a10a93613cbd044f0f933a5
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-06-10 13:46:35 +0200

    fix exercise save

--- server/collections/exercises.js
+++ server/collections/exercises.js
@@ -9,11 +9,7 @@
 						programmingLanguage: String,
 						category_id: String,
 						difficulty: Match.Integer,
-						type: Match.Integer,
-						names: [Match.ObjectIncluding({ language: String, name: String })],
-						descriptions: [Match.ObjectIncluding({ language: String, description: String })],
-						functions: [Match.ObjectIncluding({ name: String, inputNames: [String], inputTypes: [String], outputNames: [String], outputTypes: [String] })],
-						testCases: [Match.ObjectIncluding({ functionName: String, inputValues: [String], expectedOutputValues: [String] })]
+						type: Match.Integer
 					}));
 
 				const _exercise = exercise._id ? Progressor.exercises.findOne({ _id: exercise._id }) : exercise;
