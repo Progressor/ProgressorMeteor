434d9440e43439aa3cd8c2e0428105ea4f0897f6
Thomas Reto Strub <thomas.strub@students.bfh.ch>
2016-06-13 16:38:13 +0200

    add template to container

--- client/templates/router/layout.js
+++ client/templates/router/layout.js
@@ -14,6 +14,7 @@
 			language: () => i18n.getLanguages()[i18n.getLanguage()],
 			languages: () => _.map(i18n.getLanguages(), (l, i) =>({ _id: i, name: l, isActive: i === i18n.getLanguage() })),
 			currentUserEmail: () => Progressor.getUserEmail(Meteor.user()),
+			currentTemplate: () => Router.current().route.getName(),
 			i18nProgrammingLanguages: () => _.map(Progressor.getProgrammingLanguages(), l => _.extend({ name: i18n.getProgrammingLanguage(l._id) }, l)),
 			i18nExerciseTypes: () => _.map(Progressor.getExerciseTypes(), t => _.extend({ name: i18n.getExerciseType(t._id) }, t)),
 			nofExercisesToRelease() {
