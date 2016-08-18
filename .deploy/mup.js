cb4e984f5eb93750d3a1e35027b1ef6b2de3be45
Thomas Reto Strub <ths@strubt.ch>
2016-08-19 00:27:31 +0200

    upgrade to kadirahq/meteor-up

---
+++ .deploy/mup.js
@@ -0,0 +1,32 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '<meteor-host>',
+      username: '<meteor-user>',
+      password: '<meteor-password>'
+    }
+  },
+  meteor: {
+    name: 'progressor',
+    path: '..',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: '<meteor-url>',
+      MONGO_URL: '<mongo-url>'
+    },
+    dockerImage: 'abernix/meteord:base',
+    deployCheckWaitTime: 60
+  },
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
