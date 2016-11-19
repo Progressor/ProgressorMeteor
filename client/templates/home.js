7cb3344c421b514d12ccad75b2f4878debb58905
Michael Luggen <github.michael@oiu.ch>
2016-11-19 14:05:23 +0100

    reworked homepage layout

--- client/templates/home.js
+++ client/templates/home.js
@@ -7,13 +7,11 @@ const introTexts = [
 
 Template.home.onRendered(function () {
   this.introIndex = 0;
+  $('#logo').text('');
   this.introInterval = Meteor.setInterval(() => {
     if (this.introIndex < introTexts[0].length) {
-      this.$('#intro').text(introTexts[0].substr(0, ++this.introIndex)); // animate title
+      $('#logo').text(introTexts[0].substr(0, ++this.introIndex)); // animate title
     } else if (this.introIndex < introTexts[0].length + introTexts[1].length) {
-      if (this.introIndex === introTexts[0].length) {
-        $('<small id="introExplanation"></small>').appendTo(this.$('#intro')); // create & animate explanation
-      }
       this.$('#introExplanation').text(introTexts[1].substr(0, ++this.introIndex - introTexts[0].length));
     } else {
       Meteor.clearInterval(this.introInterval); // clear animation interval
@@ -28,14 +26,7 @@ Template.home.onDestroyed(function templateHomeOnDestroyed() {
 
 // PROGRAMMING LANGUAGE VISUALISATION //
 
-const cols = { xs: 6, sm: 4, md: 3, lg: 2 };
-
-function getSeparators(idx) {
-  return _.filter(_.map(cols, (no, cod) => ((idx + 1) % (12 / no) === 0 ? cod : null)), cod => !!cod);
-}
-
 Template.home.helpers({
-  nofColumns: cod => cols[cod],
   programmingLanguages() {
     const cur = Progressor.getProgrammingLanguages();
     const upc = Progressor.getProgrammingLanguagesUpcoming();
@@ -43,12 +34,10 @@ Template.home.helpers({
       _.map(cur, (lng, idx) => _.extend({
         name: i18n.getProgrammingLanguage(lng._id),
         description: i18n.getProgrammingLanguageDescription(lng._id),
-        separators: getSeparators(idx),
       }, lng)),
       _.map(upc, (lng, idx) => _.extend({
         name: i18n.getProgrammingLanguage(lng._id),
         description: i18n.getProgrammingLanguageDescription(lng._id),
-        separators: getSeparators(cur.length + idx),
         isUpcoming: true,
       }, lng)));
   },
