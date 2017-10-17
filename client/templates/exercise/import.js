e1460684ae3dcfb77b7d4887449f4916c05f4d55
Thomas Reto Strub <ths@strubt.ch>
2017-10-17 16:44:52 +0200

    #20: add basic functionality (cannot re-assign category yet)

--- client/templates/exercise/import.js
+++ client/templates/exercise/import.js
@@ -1,33 +1,72 @@
+import { tmpl } from '/imports/utilities';
+
+Template.exerciseImport.onCreated(function () {
+  // TEMPLATE VARIABLES //
+
+  this.files = new ReactiveVar([]);
+});
+
+Template.exerciseImport.helpers({
+  // DATA HELPERS //
+
+  files: () => tmpl().files.get(),
+  exercises: () => _.chain(tmpl().files.get()).map(file => _.map(file.exercises, exercise => ({
+      ..._.pick(file, 'exported'),
+      ..._.pick(exercise, 'names', 'type', 'programmingLanguage', 'difficulty', 'imported'),
+      category: _.pick(_.findWhere(file.categories, { _id: exercise.category_id }), 'names', 'private'),
+    }))).flatten(true).value(),
+});
 
 Template.exerciseImport.events({
-  'change #input-file'(event) {
+
+   // LOADING //
+
+  'change #input-file'(event, template) {
     const files = event.currentTarget.files;
-    _.each(files, (file) => {
+    _.each(files, file => {
       const fileReader = new FileReader();
       fileReader.onload = () => {
         const content = JSON.parse(fileReader.result);
-        const categoryMap = {};
-        _.each(content.categories, (category) => {
-          if (category.private) {
-            categoryMap[category._id] = Progressor.categories.findOne({ private: true, programmingLanguage: category.programmingLanguage })._id;
-          } else {
-            Meteor.call('saveCategory', category, Progressor.handleError((error, result) => {
-              //Progressor.showAlert(i18n('form.saveSuccessfulMessage'), 'success');
-            }, false));
-          }
-        });
-
-        _.each(content.exercises, (exercise) => {
-          if (categoryMap[exercise.category_id]) {
-            exercise.category_id = categoryMap[exercise.category_id];
-          }
-          Meteor.call('saveExercise', exercise, Progressor.handleError((error, result) => {
-            Progressor.showAlert(i18n('form.saveSuccessfulMessage'), 'success');
-          }, false));
-        });
+        template.files.set([...template.files.get(), content]);
       };
-
       fileReader.readAsText(file);
     });
+  },
+
+  // SAVING //
+
+  'click #btn-import'(event, template) {
+    const files = template.files.get();
+    const categoryMap = {};
+    const tasks = [];
+    const moveNext = object => (() => {
+      object.imported = true;
+      template.files.set(files);
+      tasks.shift()();
+    });
+
+    _.each(files, (file) => {
+      tasks.push(..._.chain(file.categories).filter(c => !c.imported).map(category => (() => {
+        if (category.private) {
+          categoryMap[category._id] = Progressor.categories.findOne({ private: true, programmingLanguage: category.programmingLanguage })._id;
+          moveNext(category)();
+        } else {
+          Meteor.call('saveCategory', category, Progressor.handleError(moveNext(category), false));
+        }
+      })).value());
+
+      tasks.push(..._.chain(file.exercises).filter(e => !e.imported).map(exercise => (() => {
+        let _exercise = exercise;
+        if (categoryMap[exercise.category_id]) {
+          _exercise = { ...exercise, category_id: categoryMap[exercise.category_id] };
+        }
+        Meteor.call('saveExercise', _exercise, Progressor.handleError(moveNext(exercise), false));
+      })).value());
+    });
+
+    if (tasks.length) {
+      tasks.push(() => Progressor.showAlert(i18n('form.saveSuccessfulMessage'), 'success'));
+      tasks.shift()();
+    }
   },
 });
--- server/collections/exercises.js
+++ server/collections/exercises.js
@@ -13,7 +13,7 @@ Meteor.methods({
       type: Match.Integer,
     }));
 
-    const _exercise = exercise._id ? Progressor.exercises.findOne({ _id: exercise._id }) : exercise;
+    const _exercise = (exercise._id ? Progressor.exercises.findOne({ _id: exercise._id }) : undefined) || exercise;
 
     if (!this.userId) {
       throw new Meteor.Error('not-authenticated', i18n.forUser('error.notAuthenticated.message', this.userId));
@@ -23,7 +23,7 @@ Meteor.methods({
       throw new Meteor.Error('not-admin', i18n.forUser('error.notAdmin.message', this.userId));
     }
 
-    if (!exercise.author_id) {
+    if (!exercise.author_id || !Meteor.user(exercise.author_id)) {
       exercise.author_id = this.userId;
     }
     exercise.lastEditor_id = this.userId;
